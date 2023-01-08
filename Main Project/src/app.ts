2257c97113f390cbe631853a3f572389484402cf
Leonardo Rodrigues <leonardoacrg@gmail.com>
2023-01-08 04:22:08 -0300

    preparing to deploy on render

--- Main Project/src/app.ts
+++ Main Project/src/app.ts
@@ -17,11 +17,22 @@ app.locals.htmlDisplay = (html: string) =>
 //enable express to parse URL-encoded body i.e. info from HTML form
 app.use(express.urlencoded({ extended: true }));
 
-// setting up EJS
-app.set('view engine', 'ejs');
+// require node:path to replace \ to / into dirname
+// import path from 'path';
 
-// define public and static folder (js and css files)
-app.use(express.static('public'));
+// Taking __dirname and turning to string so we can change /public directory
+// const pathDIR = __dirname;
+// const formatDIR = pathDIR.split(path.sep).join('/');
+// const viewsPath = formatDIR.replace('dist', 'dist/views');
+// const publicPath = formatDIR.replace('dist', 'dist/public');
+
+// console.log("Views Path check: " + viewsPath)
+// console.log('Public Path check: ' + publicPath);
+// setting up EJS
+// app.set('views', viewsPath);
+// app.set('views', ['./../views/week-days', './../views']);
+app.set('view engine', 'ejs'); // setting up EJS
+app.use(express.static('public')); // define public and static folder (js and css files)
 
 // routes
 app.use(mainRoutes);
